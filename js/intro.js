3a9ffbd8e10a99be17fa8f89540115ebfd3cf3bf
Shur <shuryeruult03@gmail.com>
2023-09-07 01:26:37 +0800

    Good

--- js/intro.js
+++ js/intro.js
@@ -1,99 +1,45 @@
-let list = document.querySelector('.list');
-let products = [
-    {
-        name: "ProArt Studiobook",
-        year: "2022",
-        price: "2000000",
-        img: "../img/proArt.png",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    },
-    {
-        name: "Zenbook Pro",
-        year: "2023",
-        price: "3500000",
-        img: "../img/zenbook.png",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    },
-    {
-        name: "ExpertBook B5",
-        year: "2021",
-        price: "2000000",
-        img: "../img/expert.webp",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    },
-    {
-        name: "ASUS Chrome book CX9",
-        year: "2022",
-        price: "2700000",
-        img: "../img/chrome.png",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    },
-    {
-        name: "ASUS TUF Gaming F15",
-        year: "2023",
-        price: "3400000",
-        img: "../img/gaming.png",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    },
-    {
-        name: "ProArt Studiobook 16",
-        year: "2022",
-        price: "1200000",
-        img: "../img/vivo.png",
-        def: "",
-        mouse: "../img/mouse.jpg",
-        backpack: "../img/backpack.jpd",
-        virus: "../img/virus.png",
-        office: "../img/office.jpg",
-        oneDrive: "../img/oneDrive.jpg"
-    }
+let contact = document.querySelector('.list');
+let info = [{
+    name: 'Хаяг',
+    text: 'Хан-Уул дүүрэг',
+    links: 'github.com',
+    logo: 'fa-solid fa-location-dot',
+    idName: 'address'
+},
+{
+    name: 'Facebook',
+    text: 'Computer shop Mongolia',
+    links: 'https://www.facebook.com/computershop.mn/',
+    logo: 'fa-brands fa-facebook>',
+    idName: 'fb'
+},
+{
+    name: 'Instagram',
+    text: 'Computer Shop',
+    links: 'https://www.instagram.com/computershop.mn/',
+    logo: 'fa-regular fa-circle-check',
+    idName: 'ig'
+},
+{
+    name: 'Утас',
+    text: '86843309',
+    links: 'telecom.mn',
+    logo: 'fa-solid fa-phone-volume',
+    idName: 'telphone'
+}
 ];
-let categories = [...new Set(products.map((item) => { return item }))];
-let cart = document.getElementById('def');
-cart.innerHTML = categories.map((item) => {
-    var { name, price, img } = item;
-    return (`<article class="content">
-            
-    <img class="images" src=${img}>
-                    <h3>${name} </h3>
-                    <p> Тайлбар хэсэг оруулах</p>
-                    <h6>${price}</h6>
-                    <ul>
-                        <li><i class="fa fa-star checked" aria-hidden="true"> </i></li>
-                        <li><i class="fa fa-star checked" aria-hidden="true"> </i></li>
-                        <li><i class="fa fa-star checked" aria-hidden="true"> </i></li>
-                        <li><i class="fa fa-star checked" aria-hidden="true"> </i></li>
-                        <li><i class="fa fa-star " aria-hidden="true"> </i></li>
-                    </ul>
-               
-                <button class="buy" onclick="addCard()">Add to cart</button>
-            </article>`
-    )
-
-
+let categories = [... new Set(info.map((item) => { return item }))];
+let sort = document.getElementById('contactInfo');
+sort.innerHTML = categories.map((item) => {
+    var { name, text, links, logo, idName } = item;
+    return (`<div class="box">
+    <div class="icon">
+    <i id='${idName}' class='${logo}'></i>
+    <a href=${links}></a>
+    </div>
+    <div class="text">
+        <h3>${name}</h3>
+        <p>${text}</p>
+    </div>
+</div>`)
 }).join('')
